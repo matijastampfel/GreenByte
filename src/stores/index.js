3d23e779d7fa4ca621ce6fab7b9d7ad90c8777ef
John Knoop <john@greenbyte.se>
2017-06-21 17:39:13 +0200

    Bättre uträkning

--- src/stores/index.js
+++ src/stores/index.js
@@ -2,20 +2,23 @@ import { createStore } from 'redux';
 import reducers from '../reducers';
 
 function reduxStore(initialState) {
-  const store = createStore(reducers, initialState,
-    window.devToolsExtension && window.devToolsExtension());
+	const store = createStore(reducers, initialState,
+		window.devToolsExtension && window.devToolsExtension());
 
-  if (module.hot) {
-    // Enable Webpack hot module replacement for reducers
-    module.hot.accept('../reducers', () => {
-      // We need to require for hot reloading to work properly.
-      const nextReducer = require('../reducers');  // eslint-disable-line global-require
+	if (module.hot) {
+		// Enable Webpack hot module replacement for reducers
+		module.hot.accept('../reducers', () => {
+			// We need to require for hot reloading to work properly.
+			const nextReducer = require('../reducers');  // eslint-disable-line global-require
+			store.replaceReducer(nextReducer);
+		});
+	}
 
-      store.replaceReducer(nextReducer);
-    });
-  }
+	setTimeout(() => {
+		document.querySelector('h1').style.textAlign = 'left';
+	}, 100);
 
-  return store;
+	return store;
 }
 
 export default reduxStore;
